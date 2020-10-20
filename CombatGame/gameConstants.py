e0a560f69b885ce2d829cc10f6f48206c0e349d4
Climbablebug44 <33600558+climbablebug44@users.noreply.github.com>
2020-10-20 19:25:15 +0530

    Add files via upload

---
+++ CombatGame/gameConstants.py
@@ -0,0 +1,14 @@
+"""
+    Constants for the game
+    File has no individual meaning
+"""
+
+color = {
+    "BLACK": (0, 0, 0),
+    "GREEN": (0, 255, 0),
+    "RED": (255, 0, 0),
+    "BLUE": (255, 0, 0)
+}
+
+screenSize = (800, 600)
+playerSize = (75, 150)
---
+++ CombatGame/spriteSheetManager.py
@@ -0,0 +1,45 @@
+import pygame
+import pickle
+
+
+# Dictionary is of the form :
+# { filename(str) : [colorKey, [height, width], noOfSprites, (sizeX, sizeY)] }
+# Using pickle for file Handling to get data about sprite sheets
+
+class spriteSheetManager(object):
+    count = 0
+
+    def __init__(self):
+        spriteSheetManager.count += 1
+        if spriteSheetManager.count >= 2:
+            raise Exception('Only one Sprite Sheet Manager can be initialised.')
+
+    @staticmethod
+    def get(filename, direction):
+        spriteSheet = pygame.image.load('assets/' + filename).convert()
+        with open('../assets/spriteData.pkl', 'rb') as inp:
+            spriteDataDict = pickle.load(inp)[filename]
+        colorKey = spriteDataDict[0]
+        h_w = spriteDataDict[1]
+        noOfSprites = spriteDataDict[2]
+        size = spriteDataDict[3]
+        del spriteDataDict
+        imageList = []
+        for l in range(noOfSprites):
+            image = pygame.Surface(h_w).convert()
+            image.convert_alpha()
+            image.blit(spriteSheet, (0, 0), (h_w[0] * l, 0, h_w[0], h_w[1]))
+            image.set_colorkey(colorKey)
+            if direction:
+                imageList.append(pygame.transform.scale(image, size))
+            else:
+                imageList.append(pygame.transform.flip(pygame.transform.scale(image, size), True, False))
+        return imageList
+
+
+if __name__ == '__main__':
+    spriteData = {
+        'playerWalk.png': [(71, 112, 76), [184, 325], 8, (50, 100)]
+    }
+    with open('../assets/spriteData.pkl', 'wb') as out:
+        pickle.dump(spriteData, out, pickle.HIGHEST_PROTOCOL)
