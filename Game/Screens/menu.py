4db5973dce181c332d0e57d4653a1190c4b97fb5
aashish-fx <73184707+aashish-fx@users.noreply.github.com>
2020-10-22 18:33:20 +0530

    Update menu.py

--- Game/Screens/menu.py
+++ Game/Screens/menu.py
@@ -2,7 +2,7 @@ import pygame
 from CombatSystem.combatGame import combatGame
 
 
-class Menu:
+class Menu():
     def __init__(self, game):
         self.game = game
         self.mid_w, self.mid_h = self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2
@@ -11,21 +11,20 @@ class Menu:
         self.offset = - 100
 
     def draw_cursor(self):
-        self.game.draw_text('->', 14, self.cursor_rect.x, self.cursor_rect.y)
+        self.game.draw_text('(-)', 20, self.cursor_rect.x, self.cursor_rect.y) 
 
     def blit_screen(self):
         self.game.window.blit(self.game.display, (0, 0))
         pygame.display.update()
         self.game.reset_keys()
 
-
 class MainMenu(Menu):
     def __init__(self, game):
         Menu.__init__(self, game)
         self.state = "Start"
         self.startx, self.starty = self.mid_w, self.mid_h + 30
-        self.optionsx, self.optionsy = self.mid_w, self.mid_h + 50
-        self.creditsx, self.creditsy = self.mid_w, self.mid_h + 70
+        self.optionsx, self.optionsy = self.mid_w, self.mid_h + 65
+        self.creditsx, self.creditsy = self.mid_w, self.mid_h + 100
         self.cursor_rect.midtop = (self.startx + self.offset, self.starty)
 
     def display_menu(self):
@@ -34,19 +33,20 @@ class MainMenu(Menu):
             self.game.check_events()
             self.check_input()
             self.game.display.fill(self.game.BLACK)
-            self.game.draw_text('Main Menu', 15, self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2 - 20)
-            self.game.draw_text("Start Game", 15, self.startx, self.starty)
-            self.game.draw_text("Options", 15, self.optionsx, self.optionsy)
-            self.game.draw_text("Credits", 15, self.creditsx, self.creditsy)
+            self.game.draw_text('Main Menu', 35, self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2 - 25)
+            self.game.draw_text("Start Game", 25, self.startx, self.starty)
+            self.game.draw_text("Settings", 25, self.optionsx, self.optionsy)
+            self.game.draw_text("Credits", 25, self.creditsx, self.creditsy)
             self.draw_cursor()
             self.blit_screen()
+
 
     def move_cursor(self):
         if self.game.DOWN_KEY:
             if self.state == 'Start':
                 self.cursor_rect.midtop = (self.optionsx + self.offset, self.optionsy)
-                self.state = 'Options'
-            elif self.state == 'Options':
+                self.state = 'Settings'
+            elif self.state == 'Settings':
                 self.cursor_rect.midtop = (self.creditsx + self.offset, self.creditsy)
                 self.state = 'Credits'
             elif self.state == 'Credits':
@@ -56,32 +56,30 @@ class MainMenu(Menu):
             if self.state == 'Start':
                 self.cursor_rect.midtop = (self.creditsx + self.offset, self.creditsy)
                 self.state = 'Credits'
-            elif self.state == 'Options':
+            elif self.state == 'Settings':
                 self.cursor_rect.midtop = (self.startx + self.offset, self.starty)
                 self.state = 'Start'
             elif self.state == 'Credits':
                 self.cursor_rect.midtop = (self.optionsx + self.offset, self.optionsy)
-                self.state = 'Options'
+                self.state = 'Settings'
 
     def check_input(self):
         self.move_cursor()
         if self.game.START_KEY:
             if self.state == 'Start':
                 self.game.playing = True
-                # x = combatGame()
-            elif self.state == 'Options':
+            elif self.state == 'Settings':
                 self.game.curr_menu = self.game.options
             elif self.state == 'Credits':
                 self.game.curr_menu = self.game.credits
             self.run_display = False
-
 
 class OptionsMenu(Menu):
     def __init__(self, game):
         Menu.__init__(self, game)
         self.state = 'Volume'
         self.volx, self.voly = self.mid_w, self.mid_h + 20
-        self.controlsx, self.controlsy = self.mid_w, self.mid_h + 40
+        self.controlsx, self.controlsy = self.mid_w, self.mid_h + 45
         self.cursor_rect.midtop = (self.volx + self.offset, self.voly)
 
     def display_menu(self):
@@ -90,9 +88,10 @@ class OptionsMenu(Menu):
             self.game.check_events()
             self.check_input()
             self.game.display.fill((0, 0, 0))
-            self.game.draw_text('Options', 20, self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2 - 30)
-            self.game.draw_text("Volume", 15, self.volx, self.voly)
-            self.game.draw_text("Controls", 15, self.controlsx, self.controlsy)
+            self.game.draw_text('Settings', 32, self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2 - 30)
+            self.game.draw_text("Volume", 22, self.volx, self.voly)
+            self.game.draw_text("Controls", 22, self.controlsx, self.controlsy)
+            self.game.draw_text("Language",22,self.mid_w, self.mid_h + 67)
             self.draw_cursor()
             self.blit_screen()
 
@@ -105,12 +104,14 @@ class OptionsMenu(Menu):
                 self.state = 'Controls'
                 self.cursor_rect.midtop = (self.controlsx + self.offset, self.controlsy)
             elif self.state == 'Controls':
+                self.state = 'Language'
+                self.cursor_rect.midtop = (self.mid_w + self.offset, self.mid_h + 67) 
+            elif self.state == 'Language':
                 self.state = 'Volume'
                 self.cursor_rect.midtop = (self.volx + self.offset, self.voly)
         elif self.game.START_KEY:
             # TO-DO: Create a Volume Menu and a Controls Menu
             pass
-
 
 class CreditsMenu(Menu):
     def __init__(self, game):
@@ -127,3 +128,5 @@ class CreditsMenu(Menu):
             self.game.draw_text('Credits', 20, self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2 - 20)
             self.game.draw_text('By team Synergy', 15, self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2 + 10)
             self.blit_screen()
+
+
