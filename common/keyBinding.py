916959f42c3e3aff0816b40b23fcd0aa8dc40514
Abhimanyu Kumbhar <61832392+Abhimanyu-K@users.noreply.github.com>
2020-10-27 20:10:51 +0530

    basic scode structure
    code structure for key binding file handling
    more changes are to be made

--- common/keyBinding.py
+++ common/keyBinding.py
@@ -1,1 +1,17 @@
-
+class temp:
+  
+  self.keys = ['W', 'S', 'A', 'D', 'P', 'C', 'U', 'I', 'Space', 'E', 'B', 'Q', 'Done']
+  
+  def __init__(self):
+    self.arr = self.keys
+  
+  def funct1(self):
+    file1 = open("file.txt",w+")
+    file1.write(self.arr)
+    file1.close() 
+  
+  def funct2(self, repl):
+    self.arr = repl
+    file1 = open("file.txt",w+")
+    file1.write(self.arr)
+    file1.close() 
