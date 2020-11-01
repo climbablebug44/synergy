8bb5ccf46c81b41fa999522cce9a872c5846928d
Nimit14 <58770491+Nimit14@users.noreply.github.com>
2020-11-01 18:59:12 +0530

    Create spriteDetails.py

---
+++ Animation/spriteDetails.py
@@ -0,0 +1,13 @@
+import pygame
+
+pygame.init()
+
+
+class player(object):
+
+    filename = 'assets/.png'
+    sizeIn = (0, 0)
+    sizeOut = (0, 0)
+    colorKey = (0, 0, 0)
+    noOfSprites = 0
+    direction = False
