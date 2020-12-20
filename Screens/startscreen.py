578cc456513585c36af4d09bb0f0ac3ad681f0b9
Climbablebug44 <33600558+climbablebug44@users.noreply.github.com>
2020-12-20 21:37:14 +0530

    minorchanges

--- Screens/startscreen.py
+++ Screens/startscreen.py
@@ -33,15 +33,19 @@ class startScreen(pygame.Surface):
         self.pageLabels = {'mainpage': ['Play', 'Settings', 'Credits', 'Exit'],
                            'settings': ['Walk Left', 'Walk Right', 'Block', 'Light Attack', 'Heavy Attack',
                                         'Slot toggle Up', 'Slot Toggle Down', 'Jump', 'Reload', 'Special', 'Push',
-                                        'Auto-Aim Shoot', 'Toggle Auto aim', 'Toggle selected enemy']}
+                                        'Auto-Aim Shoot', 'Toggle Auto aim', 'Toggle selected enemy'],
+                           'pause': ['Resume', 'Settings', 'Quit to Main Menu']}
         self.keyBind = keyBinding()
 
         self.defaultSize = {'mainpage': 30,
-                            'settings': 30}
+                            'settings': 30,
+                            'pause': 30}
         self.offset = {'mainpage': 30,
-                       'settings': 30}
+                       'settings': 20,
+                       'pause': 10}
         self.static = 0
         self.highlightMemory = -1
+        self.history = self.mode
 
         while self.running:
             self.screen.fill(self.theme[1])
@@ -55,6 +59,9 @@ class startScreen(pygame.Surface):
                 raise Exception('Invalid mode:' + self.mode)
             pygame.display.flip()
             self.clock.tick(60)
+
+    def changeFontSize(self, x):
+        self.font = pygame.font.Font('freesansbold.ttf', x)
 
     def drawText(self, text, position, color=()):
         text = self.font.render(text, True, self.theme[0])
@@ -84,6 +91,7 @@ class startScreen(pygame.Surface):
         return tuple(map(lambda x, y: x + y, a, b))
 
     def drawMainPage(self):
+        self.changeFontSize(30)
         center = (self.screenSize[0] / 2, self.screenSize[1] / 2)
         for j in range(len(self.pageLabels[self.mode])):
             color = ()
@@ -113,30 +121,41 @@ class startScreen(pygame.Surface):
                 self.resetEverything()
 
     def drawSettings(self):
+        self.changeFontSize(40)
         key = self.keyBind.returnBindings()
         self.screen.fill(gc.color['BLACK'])
+        self.drawText('Settings', (self.screenSize[0] // 2, 50))
+        self.drawText('Controls', (250, 100))
+        self.changeFontSize(15)
         for i in range(len(self.pageLabels[self.mode])):
             highlight = ()
             if i == self.selected:
                 highlight = (100, 100, 100)
-            self.drawText(self.pageLabels[self.mode][i], self.tuplePlus((150, 100), (150, 50 * i)), highlight)
+            self.drawText(self.pageLabels[self.mode][i], self.tuplePlus((100, 150), (150, 30 * i)), highlight)
             if i == self.highlightMemory:
                 highlight = (128, 0, 0)
             else:
                 highlight = ()
-            self.drawText(pygame.key.name(key[i]), self.tuplePlus((500, 100), (500, 50 * i)), highlight)
+            self.drawText(pygame.key.name(key[i]), self.tuplePlus((450, 150), (500, 30 * i)), highlight)
+        self.drawText('Press Backspace to save and exit, or Esc to reset everything back to default...',
+                      (300, self.screenSize[1] - 15))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                exit()
+                # TODO: ESCAPE KEY WORKS ONLY ONCE
+                print('hello')
+                self.keyBind.setDefault()
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_BACKSPACE:
-                self.mode = 'mainpage'
+                self.keyBind.commit()
+                self.mode = self.history
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
                 self.selected = (self.selected - 1) % len(self.pageLabels[self.mode])
                 self.static = 0
+                self.highlightMemory = -1
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
                 self.static = 0
+                self.highlightMemory = -1
                 self.selected = (self.selected + 1) % len(self.pageLabels[self.mode])
             elif event.type == pygame.KEYDOWN:
                 self.highlightMemory = -1
@@ -147,14 +166,41 @@ class startScreen(pygame.Surface):
                 if tempkeys != sorted(list(set(key))):
                     self.highlightMemory = self.selected
                     key[self.selected] = temp
-        self.keyBind.change(key)
-        self.keyBind.commit()
+                self.keyBind.change(key)
+            else:
+                break
 
     def drawPauseScreen(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+                self.selected = (self.selected + 1) % len(self.pageLabels[self.mode])
+                self.static = 0
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+                self.selected = (self.selected - 1) % len(self.pageLabels[self.mode])
+                self.static = 0
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                if self.selected == 0:
+                    self.running = False
+                elif self.selected == 1:
+                    self.resetEverything()
+                    self.history = self.mode
+                    self.mode = 'settings'
+                elif self.selected == 2:
+                    self.resetEverything()
+                    # TODO: SAVE GAME
+                    self.history = 'mainpage'
+                    self.mode = 'mainpage'
+
         self.screen.fill(gc.color['BLACK'])
+        self.changeFontSize(20)
+        for i in range(len(self.pageLabels[self.mode])):
+            color = ()
+            if self.selected == i:
+                color = (100, 100, 100)
+            self.drawText(self.pageLabels[self.mode][i],
+                          self.tuplePlus((self.screenSize[0] // 2, self.screenSize[1] // 2), (0, 30 * i)), color)
 
 
 sizeofScr = (800, 600)
@@ -164,4 +210,4 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
     sizeofScr = screen.get_size()
     clock = pygame.time.Clock()
-    start = startScreen(sizeofScr, screen, clock)
+    start = startScreen(sizeofScr, screen, clock, mode='pause')
--- common/keyBinding.py
+++ common/keyBinding.py
@@ -18,7 +18,10 @@ class keyBinding:
 
     def change(self, newL):
         self.bindings = newL
-        self.commit()
+
+    def setDefault(self):
+        self.bindings = [pygame.K_a, pygame.K_d, pygame.K_b, pygame.K_q, pygame.K_e, pygame.K_w, pygame.K_s,
+                         pygame.K_SPACE, pygame.K_r, pygame.K_f, pygame.K_t, pygame.K_z, 0, pygame.K_c]
 
     def commit(self, tempPath=''):
         if tempPath == '':
